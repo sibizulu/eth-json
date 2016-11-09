a064b3f098e832ba745793762fcb3ff42b83504f
sibizulu <sibiraj@lightrains.com>
2016-11-09 13:27:55 +0530

    Adding module functions

--- eth-json.js
+++ eth-json.js
@@ -1,27 +1,36 @@
-'use strict';
-var ethJson = function(address){
-  this.pubKey = address;
-}
-ethJson.generate = function(){
-  this.generateJSON();
-}
-ethJson.prototype.generateJSON = function() {
-  return {
-    address: this.getAddressString(),
-    publisher:"ethJson",
-    encrypted:false,
-    version:2
-  }
-}
-ethJson.prototype.getAddressString = function() {
-  return '0x' + this.getAddress().toString('hex')
-}
-ethJson.prototype.getAddress = function() {
-  if (typeof this.pubKey == "undefined") {
-    return ethUtil.privateToAddress(this.privKey)
-  }
-  else {
-    return ethUtil.publicToAddress(this.pubKey, true)
-  }
-}
-module.exports = ethJson;
+(function() {
+  var ethJson = (function() {
+
+    var ethJson = function(address){
+      this.pubKey = address;
+    }
+    ethJson.prototype.generateJSON = function() {
+      return {
+        address: this.getAddressString(),
+        publisher:"ethJson",
+        encrypted:false,
+        version:2
+      }
+    }
+    ethJson.prototype.getAddressString = function() {
+      return '0x' + this.pubKey
+    }
+    ethJson.prototype.getAddress = function() {
+      if (typeof this.pubKey == "undefined") {
+        return ethUtil.privateToAddress(this.privKey)
+      }
+      else {
+        return ethUtil.publicToAddress(this.pubKey, true)
+      }
+    }
+  })();
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+  module.exports = ethJson;
+else
+  window.ethJson = ethJson;
+
+})();
+
+
+
