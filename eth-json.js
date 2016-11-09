fde8e176b7e0448c6d2e56effe81c60b41f10005
sibizulu <sibiraj@lightrains.com>
2016-11-09 11:59:51 +0530

    Adding json format

--- eth-json.js
+++ eth-json.js
@@ -1,6 +1,27 @@
-(function(window){
-
-  'use strict';
-
-
-})(window);
+'use strict';
+var ethJson = function(address){
+  this.pubKey = address;
+}
+ethJson.generate = function(){
+  this.generateJSON();
+}
+ethJson.prototype.generateJSON = function() {
+  return {
+    address: this.getAddressString(),
+    publisher:"ethJson",
+    encrypted:false,
+    version:2
+  }
+}
+ethJson.prototype.getAddressString = function() {
+  return '0x' + this.getAddress().toString('hex')
+}
+ethJson.prototype.getAddress = function() {
+  if (typeof this.pubKey == "undefined") {
+    return ethUtil.privateToAddress(this.privKey)
+  }
+  else {
+    return ethUtil.publicToAddress(this.pubKey, true)
+  }
+}
+module.exports = ethJson;
