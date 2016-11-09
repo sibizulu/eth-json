5f55ffd2592768a44c80b2294235a7a21c4f770f
sibizulu <sibiraj@lightrains.com>
2016-11-09 17:24:04 +0530

    Adding js object

--- eth-json.js
+++ eth-json.js
@@ -1,36 +1,34 @@
-(function() {
-  var ethJson = (function() {
+var ethJson = {
+  jsonGem: {
+    "address":"",
+    "checksumAddress":"",
+    "privKey":"",
+    "pubKey":"",
+    "publisher":"ethJson",
+    "encrypted":false,
+    "version":2
+  },
+  init: function(address){
+    this.buildData(address)
+    this.elementCatch()
+    this.render()
+  },
+  buildData: function(address){
+    this.jsonGem.address = "0x" + address || "0x"
+    this.jsonGem.pubKey = "0x" + address || "0x"
+    this.jsonGem.pubKey = "0x" + address.toString(16) || "0x"
+  },
+  elementCatch: function(){
+    this.el = document.getElementById("target");
+    this.template = document.getElementById("template").innerHTML;
+  },
+  render: function(){
+    var data = {
+      jsonGem: this.jsonGem,
+    };
+    console.log(data);
+    this.el.innerHTML = Mustache.render(this.template, data);
+  }
+}
 
-    var ethJson = function(address){
-      this.pubKey = address;
-    }
-    ethJson.prototype.generateJSON = function() {
-      return {
-        address: this.getAddressString(),
-        publisher:"ethJson",
-        encrypted:false,
-        version:2
-      }
-    }
-    ethJson.prototype.getAddressString = function() {
-      return '0x' + this.pubKey
-    }
-    ethJson.prototype.getAddress = function() {
-      if (typeof this.pubKey == "undefined") {
-        return ethUtil.privateToAddress(this.privKey)
-      }
-      else {
-        return ethUtil.publicToAddress(this.pubKey, true)
-      }
-    }
-  })();
-
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
-  module.exports = ethJson;
-else
-  window.ethJson = ethJson;
-
-})();
-
-
-
+ethJson.init("032e29Af6573a89adDca8D400cEc7a4C38B5a685");
